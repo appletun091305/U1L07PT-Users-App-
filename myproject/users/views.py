51602bde0fd64308a23303349ba90aaca8cd06e0
ll <lauliu26@bergen.org>
2024-10-17 10:27:21 -0400

    finished - users app

--- myproject/users/views.py
+++ myproject/users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def register(request):
+    # return HttpResponse(slug)
+    context = {
+        'active_link': 'users/register'
+    }
+    return render(request, 'register.html', context)
